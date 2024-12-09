0a698fd246924670bd9158d0d38eb031c815040e
Guruveer <guruwali@gmail.com>
2024-12-09 17:24:40 +0000

    Create locals.tf

---
+++ discovery_infrastructure/Terraform/mid_server/tfvars/locals.tf
@@ -0,0 +1,12 @@
+/*locals {
+
+  tags = {
+    "business_unit"      = "NBS10"
+    "product_code"       = "CA049"
+    "application_id"     = "ServiceNow Cloud Discovery"
+    "cost_centre"        = "V317"
+    "support_team_owner" = "Stephen Plant"
+    "environment"        = "Dev"
+    "project_code"       = "A622303"
+  }
+}*/
